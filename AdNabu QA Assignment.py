4e02be4356676260d790e7ed0e6f2bec5b8e683d
raunak1600 <101035865+raunak1600@users.noreply.github.com>
2026-05-13 23:02:02 +0530

    Add files via upload

---
+++ AdNabu QA Assignment.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+driver = webdriver.Edge()
+driver.maximize_window()
+driver.get("https://adnabu-store-assignment1.myshopify.com")
+driver.find_element(By.ID, "password").send_keys("AdNabuQA")
+print("Entered Password")
+driver.find_element(By.XPATH,"//button[@type='submit']").click()
+print("Clicked on Enter button")
+driver.find_element(By.XPATH,"//details-modal[@class='header__search']").click()
+print("Clicked on Search")
+driver.find_element(By.ID, "Search-In-Modal").send_keys("snowboard")
+print("Entered Snowboard")
+driver.find_element(By.XPATH,"//button[@aria-label='Search']").click()
+print("Clicked on Search Icon")
+element = driver.find_element(By.XPATH,"(//div[@class='card__content'])[3]")
+driver.execute_script("arguments[0].scrollIntoView();",element)
+driver.find_element(By.LINK_TEXT,"The Complete Snowboard").click()
+print("Product-1")
+driver.find_element(By.XPATH,"//button[@name='add']").click()
+print("Clicked on Add to Cart")
+driver.quit()
